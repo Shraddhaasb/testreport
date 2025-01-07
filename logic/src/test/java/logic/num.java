6cd798103792baec1b65eafad2f85e75cd8a4761
Shraddha <shraddhaab6@gmail.com>
2025-01-07 22:15:16 +0530

    ggg

---
+++ logic/src/test/java/logic/num.java
@@ -0,0 +1,11 @@
+package logic;
+
+public class num {
+public static void main(String[] args) {
+	int num=7;
+	
+	System.out.println(num);
+}
+	int num=7;
+	
+}
---
+++ reaal/src/reaal/mm.java
@@ -0,0 +1,5 @@
+package reaal;
+
+public class mm {
+
+}
